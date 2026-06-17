62257e7edd6f1bcb1445481a1b350a95889a6c26
Zen Dharma <zendharma10@gmail.com>
2026-06-17 18:32:06 +0800

    Update fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,25 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -U main f07427
-set -U text ffffff
-
-set -U fish_greeting ""
-set -U fish_color_user $main
-set -U fish_color_cwd $text
+set -g fish_greeting ""
+set -g fish_color_autosuggestion e0d8d0 --italic
+set -g fish_color_command f07427
+set -g fish_color_param d4a060
+set -g fish_color_operator d4a060
+set -g fish_color_redirection d4a060 --bold
+set -g fish_color_quote f0a030
+set -g fish_color_error ff5030
+set -g fish_color_comment 808080
+set -g fish_color_end f07427
+set -g fish_color_escape f07427
+set -g fish_color_prefix f07427
+set -g fish_color_user f07427
+set -g fish_color_cwd ffffff
+set -g fish_color_cancel -r
+set -g fish_color_host normal
+set -g fish_color_host_remote yellow
+set -g fish_color_normal normal
+set -g fish_color_search_match white --background=brblack --bold
+set -g fish_color_selection white --background=brblack --bold
+set -g fish_color_status red
+set -g fish_color_valid_path --underline
